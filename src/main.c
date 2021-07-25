56672e1589e8ce16a402337cc8ea188e71f72d3f
UNITY21047 <unity21047@gmail.com>
2021-07-25 00:06:08 -0500

    ./src/
    Changes to syntax style and commented out code for testing.

--- src/main.c
+++ src/main.c
@@ -3,35 +3,45 @@
 #include <stdlib.h>
 #include <string.h>
  
-int main(void) {
-   Display *d;
-   Window w;
-   XEvent e;
-   const char *msg = "Hello, World!";
-   int s;
+int main(void)
+{
+   Display* display;
+   Window window;
+   XEvent event;
+   const char *message = "Hello, World!";
+   int screen_identifier;
  
-   d = XOpenDisplay(NULL);
-   if (d == NULL) {
+   display = XOpenDisplay(NULL);
+
+   if(display == NULL) 
+   {
       fprintf(stderr, "Cannot open display\n");
       exit(1);
    }
  
-   s = DefaultScreen(d);
-   w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1,
-                           BlackPixel(d, s), WhitePixel(d, s));
-   XSelectInput(d, w, ExposureMask | KeyPressMask);
-   XMapWindow(d, w);
+   screen_identifier = DefaultScreen(display);
+   window = XCreateSimpleWindow(display, RootWindow(display, screen_identifier), 10, 10, 100, 100, 1, BlackPixel(display, screen_identifier), WhitePixel(display, screen_identifier));
+   
+   XSelectInput(display, window, ExposureMask | KeyPressMask); //Tells the x11 server to report the events in the third paremeter.
+   XMapWindow(display, window);
  
-   while (1) {
-      XNextEvent(d, &e);
-      if (e.type == Expose) {
-         XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
-         XDrawString(d, w, DefaultGC(d, s), 10, 50, msg, strlen(msg));
-      }
-      if (e.type == KeyPress)
-         break;
+   while(1)
+   {
+		XNextEvent(display, &event); //Checks if any actions have been rasied within the window.
+
+		if(event.type == Expose)
+		{
+		//Will try to figure out how the cario library organ
+		//	XFillRectangle(display, window, DefaultGC(display, screen_identifier), 20, 20, 10, 10);
+		//	XDrawString(display, window, DefaultGC(display, screen_identifier), 10, 50, message, strlen(message));
+		}
+		
+		if(event.type == KeyPress)
+		{
+			break;
+		}
    }
  
-   XCloseDisplay(d);
+   XCloseDisplay(display);
    return 0;
 }
